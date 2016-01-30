e494bac4284fb813211ccf22b6af9758ba07bfed
Dave Dong <fudaviddong@gmail.com>
2016-01-29 16:02:53 -0800

    Initial Commit

---
+++ FirebaseTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseTest
+//
+//  Created by Dave Dong on 1/29/16.
+//  Copyright © 2016 Self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
